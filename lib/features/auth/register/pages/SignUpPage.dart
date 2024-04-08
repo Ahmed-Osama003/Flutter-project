131c7cf2059cc85149fc6f34dd2d3cd9fee7ec03
Ahmedabdelaziz77 <102679746+Ahmedabdelaziz77@users.noreply.github.com>
2024-04-08 20:32:14 +0200

    Update SignUpPage.dart

--- lib/features/auth/register/pages/SignUpPage.dart
+++ lib/features/auth/register/pages/SignUpPage.dart
@@ -95,7 +95,7 @@ class SignUpPage extends StatelessWidget {
                       ),
                       style: ElevatedButton.styleFrom(
                         minimumSize: Size.fromHeight(55),
-                        backgroundColor: Color(0xFFEF6969),
+                        backgroundColor: Color(0xFFD83022),
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(8),
                         ),
